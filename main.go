bac39151a5dae73f5f42147140ec8d7719718d3a
Adam <adam@zacs-mac.attlocal.net>
2020-01-01 20:41:51 -0600

    Adding binance.WsCombinedPartialDepthServe() temp

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/adamscottbaxter/go-binance"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -23,7 +25,39 @@ func dbConn() (db *sql.DB) {
 }
 
 func main() {
-	serveWeb()
+	// serveWeb()
+	symbolLevels := map[string]string{
+		"BTCUSDT": "5",
+		"ETHUSDT": "5",
+	}
+
+	// wsDepthHandler := func(event *binance.WsDepthEvent) {
+	// 	fmt.Println(event)
+	// }
+	errHandler := func(err error) {
+		fmt.Println(err)
+	}
+
+	wsPartial := func(event *binance.WsPartialDepthEvent) {
+		fmt.Println(event)
+	}
+
+	// var wsPartialDepthEvt binance.WsPartialDepthEvent
+	// var wsPartialDepthHandler wsPartial// binance.WsPartialDepthHandler
+	// wsPartialDepthHandler := binance.WsPartialDepthHandler(func(event *wsPartialDepthEvt))
+
+	doneC, stopC, err := binance.WsCombinedPartialDepthServe(symbolLevels, wsPartial, errHandler)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// use stopC to exit
+	go func() {
+		time.Sleep(3 * time.Second)
+		stopC <- struct{}{}
+	}()
+	// remove this if you do not want to be blocked here
+	<-doneC
 }
 
 // CheckAndUpdate checks open binance orders, updates their status, then updates dishes.
